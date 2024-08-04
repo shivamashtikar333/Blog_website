f08c64a8541390f10a41d1e96fad7aeed734d0af
shivamashtikar333 <shivamaace121@gst.sies.edu.in>
2024-08-04 14:33:02 +0530

    updated app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-
+const path = require("path");
 const express = require("express");
 const expressLayout = require("express-ejs-layouts");
 const methodOverride = require("method-override");
